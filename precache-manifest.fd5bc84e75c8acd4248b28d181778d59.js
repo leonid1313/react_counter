c7a8ce96464baca73b428ea7dd7dfcc1558d4301
Leonid <manu31051996@gmail.com>
2021-07-26 14:50:00 +0300

    Updates

---
+++ precache-manifest.fd5bc84e75c8acd4248b28d181778d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731295493efc7bb0075c8d1d06e87044",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b8d5f7abe0a5f35a996",
+    "url": "./static/css/main.6d3972e5.chunk.css"
+  },
+  {
+    "revision": "7c7a19eaf806aa4e071d",
+    "url": "./static/js/2.cad20d42.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cad20d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8d5f7abe0a5f35a996",
+    "url": "./static/js/main.3c9da33c.chunk.js"
+  },
+  {
+    "revision": "2a286f7172708a7e8c5a",
+    "url": "./static/js/runtime-main.3a2c9fb2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.fd5bc84e75c8acd4248b28d181778d59.js"
 );
 
 self.addEventListener('message', (event) => {
